e0a62ea809a9d22d3f563386acf54a204e61793a
Rifat <rifatsakib353@hmail.com>
2025-01-18 22:35:14 +0600

    cors+ express  installed

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+const app = express();
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 
 
